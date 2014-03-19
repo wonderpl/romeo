57ee5058daf98f7ab625c67860278f9a439a2c45
Allan Brisbane <allan.brisbane@gmail.com>
2014-03-19 12:06:49 +0000

    Added very simple account api + docs

---
+++ docs/account.md
@@ -0,0 +1,23 @@
+Account
+=======
+
+### Account
+
+Get details for user account
+
+```http
+GET /account HTTP/1.1
+```
+
+```http
+HTTP/1.1 200 OK
+Content-Type: application/json
+
+{
+    "account": {
+        "name": "neebone", 
+        "wonder_user": "wonder user"
+    }, 
+    "username": "optimus@rockpack.com"
+}
+```
--- wonder/romeo/account/views.py
+++ wonder/romeo/account/views.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, request, render_template, url_for, redirect, jsonify
+from flask import Blueprint, request, render_template, url_for, redirect, jsonify, abort
+from flask.ext import restful
 from flask.ext.login import login_user, logout_user, fresh_login_required, current_user
+from sqlalchemy.orm import exc
 from wonder.romeo.core import dolly
+from wonder.romeo import api
 from .forms import LoginForm, ChangePasswordForm
-from .models import UserProxy
+from .models import UserProxy, AccountUser
 
 
 accountapp = Blueprint('account', __name__)
@@ -43,3 +46,21 @@ def settings():
     return render_template('account/settings.html',
                            dollyuser=dollyuser,
                            change_password_form=change_password_form)
+
+
+class AccountApi(restful.Resource):
+
+    def get(self, account_id):
+        try:
+            user = AccountUser.query.filter(AccountUser.account_id == account_id).one()
+        except exc.NoResultFound:
+            abort(404)
+        else:
+            return dict(
+                username=user.username,
+                account=dict(
+                    name=user.account.name,
+                    wonder_user=user.account.dolly_user))
+
+
+api.add_resource(AccountApi, '/account/<int:account_id>')
