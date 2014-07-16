61cc6c77d6199813fc2b62cbb102860b8d77c9c7
Paul Egan <paulegan@rockpack.com>
2014-07-16 22:51:07 +0100

    Resolved alembic conflict

--- alembic/versions/541d3e97a140_video_comment_resolved.py
+++ alembic/versions/541d3e97a140_video_comment_resolved.py
@@ -1,14 +1,14 @@
 """video_comment.resolved
 
 Revision ID: 541d3e97a140
-Revises: 3779b0b1dcbd
+Revises: 19e094c35693
 Create Date: 2014-07-16 21:56:33.403764
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '541d3e97a140'
-down_revision = '3779b0b1dcbd'
+down_revision = '19e094c35693'
 
 from alembic import op
 import sqlalchemy as sa
