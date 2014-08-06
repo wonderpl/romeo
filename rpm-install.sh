c20aa5ba7851904834a59eed07bc1a4501521750
Paul Egan <paulegan@rockpack.com>
2014-08-06 12:38:03 +0100

    Additional hack for side-by-side rpm install with wonder-common

--- rpm-install.sh
+++ rpm-install.sh
@@ -1,3 +1,7 @@
 # Clean left over files compiled with "build" path
 find build -name '*.pyc' -delete
-python2.7 setup.py  install -O1 --skip-build --root=$RPM_BUILD_ROOT --record=INSTALLED_FILES
+python2.7 setup.py  install -O1 --skip-build --root %{buildroot} --record INSTALLED_FILES
+# Exclude compiled namespace files to avoid conflict with wonder-common.
+# XXX Should really install somewhere else and fix with pth!
+sed -e '/wonder\/__init__.py[oc]/d' -i INSTALLED_FILES
+rm %{buildroot}%{python_sitelib}/wonder/__init__.py[oc]
