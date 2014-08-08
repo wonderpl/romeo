2d3d6883dd5db481ee87f0a2b8813d5382c6be30
Johan R-G <johan@wonderpl.com>
2014-08-08 12:21:42 +0100

    Fix compile/link issue

--- wonder/romeo/static/assets/scripts/directives/loading-spinner.js
+++ wonder/romeo/static/assets/scripts/directives/loading-spinner.js
@@ -4,26 +4,43 @@ angular
 
 function LoadingSpinnerDirective ($templateCache, $timeout) {
   'use strict';
+  var $interval;
+
   return {
     restrict: 'A',
     compile: function(element) {
       $(element).append('<div class="spinner" style="display: none;"><div class="spinner--wrapper"><img src="/static/assets/img/loading_white_46x46.png" class="spinner--img" /></div></div>');
       $interval = 0;
-    },
-    link: function (scope, element, attrs) {
-      element.data('$visible', attr.loadingSpinner);
-      scope.$watch($visible, function (value) {
-        $(element).find('.spinner').toggle($visible);
-        if ($interval) {
-          window.clearInterval($interval);
+      return {
+        post: function postLink(scope, element, attrs, ctrl) {
+          var visible = attrs.loadingSpinner;
+          console.group('Post link:');
+            console.log('Visible: ' + visible);
+            console.group('Element');
+              console.dir(element);
+            console.groupEnd();
+            console.group('Attributes');
+              console.dir(attrs);
+            console.groupEnd();
+          console.groupEnd();
+
+          scope.$watch(visible, function (value) {
+            console.log('Spinner value changed to: ' + visible);
+            $(element).find('.spinner').toggle(visible);
+            if ($interval) {
+              window.clearInterval($interval);
+            }
+            if (visible) {
+              var elem = $(element).find('.spinner--img');
+              $interval = window.setInterval(
+                function() {
+                  $(elem).css('top', parseInt($(elem).css('top')) < -820 ? '0px' : '-=46px');
+                }, 600
+              );
+            }
+          });
         }
-        if ($visible) {
-          var elem = $(element).find('.spinner--img');
-          $interval = window.setInterval(function() {
-            $(elem).css('top', parseInt($(elem).css('top')) < -820 ? '0px' : '-=46px');
-          }, 60);
-        }
-      });
+      };
     }
   };
 }
