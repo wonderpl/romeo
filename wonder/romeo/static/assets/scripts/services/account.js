8be3bdb69f1a8dca3f93ed5f0fea185744a2fec1
Johan R-G <johan@wonderpl.com>
2014-09-01 21:51:27 +0100

    Make AccountService method names clearer (Account not User)

--- wonder/romeo/static/assets/scripts/services/account.js
+++ wonder/romeo/static/assets/scripts/services/account.js
@@ -9,37 +9,13 @@ angular.module('RomeoApp.services').factory('AccountService',
     var debug = new DebugClass('AccountService');
 
     var Account = {},
-        User = null,
-        ID = null;
-
-    /*
-    * Returns a promise, which if resolved returns the User object from the web service.
-    */
-    Account.getUser = function() {
-        var deferred = new $q.defer();
-
-        AuthService.loginCheck().then(function(response){
-            AuthService.getSessionId().then(function (_id) {
-                if ( ID === null )
-                    debug.log('New User added to AccountService');
-                else if (_id === ID)
-                    debug.log('User already in AccountService');
-                else
-                    debug.log('User loaded into AccountService');
-
-                ID = _id;
-            });
-            User = AuthService.getUser();
-            deferred.resolve(User);
-        });
-        return deferred.promise;
-    };
+        User = null;
 
     /*
     * Used to send PATCH requests to the webservice, updating individual parts of the user account
     */
-    Account.updateUser = function(data) {
-        var url = '/api/account/' + ID;
+    Account.updateAccount = function(data) {
+        var url = '/api/account/' + Account.getAccountId();
         return DataService.request({url: url, method: 'PATCH', data: data });
     };
 
@@ -47,14 +23,14 @@ angular.module('RomeoApp.services').factory('AccountService',
     * Send a PATCH request with an updated cover image
     */
     Account.updateCoverImage = function(data) {
-        return DataService.uploadImage( ('/api/account/' + ID), 'profile_cover', data);
+        return DataService.uploadImage( ('/api/account/' + Account.getAccountId()), 'profile_cover', data);
     };
 
     /*
     * Send a PATCH request with an updated avatar image
     */
     Account.updateAvatar = function(data) {
-        return DataService.uploadImage( ('/api/account/' + ID), 'avatar', data);
+        return DataService.uploadImage( ('/api/account/' + Account.getAccountId()), 'avatar', data);
     };
 
     Account.getGeoIpLocation = function () {
@@ -75,8 +51,7 @@ angular.module('RomeoApp.services').factory('AccountService',
     * Expose the methods to the service
     */
     return {
-        getUser: Account.getUser,
-        updateUser: Account.updateUser,
+        updateAccount: Account.updateAccount,
         updateCoverImage: Account.updateCoverImage,
         updateAvatar: Account.updateAvatar,
         getGeoIpLocation: Account.getGeoIpLocation,
