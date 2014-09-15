c02f5837c0f179c4ce033d2e175f6e5e52f4b939
mr-stezz <mstezy@gmail.com>
2014-09-15 16:03:26 +0100

    Add analytics template

--- wonder/romeo/static/assets/app/analytics/analytics.js
+++ wonder/romeo/static/assets/app/analytics/analytics.js
@@ -23,4 +23,46 @@
         init();
     }
     angular.module('RomeoApp.analytics').controller('AnalyticsCtrl', ['$scope', AnalyticsCtrl]);
+
+    // bar chart data
+    var barData = {
+    labels : ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"],
+    datasets : [
+        {
+            fillColor : "#51B364",
+            strokeColor : "#000",
+            data : [78,57,70,70,40,30,58]
+        }
+        ]
+    };
+    
+    // get bar chart canvas
+    var income = document.getElementById("income").getContext("2d");
+        
+    // draw bar chart
+    new Chart(income).Bar(barData , {
+        barShowStroke: false,
+        responsive: true,
+        maintainAspectRatio: true,
+        scaleLineColor: "#f1f2f2",
+        scaleFontStyle: "bold",
+        scaleFontSize: 13,
+        scaleFontFamily: "'FreightSans Pro', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Geneva, Verdana, sans-serif",
+        scaleFontColor: "#2e2e32",
+        tooltipFillColor: "#2e2e32",
+        tooltipFontStyle: "bold",
+        tooltipFontSize: 16,
+        tooltipFontFamily: "'FreightSans Pro', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Geneva, Verdana, sans-serif",
+        tooltipFontColor: "#fff",
+        tooltipTitleFontStyle: "normal",
+        tooltipTitleFontSize: 13,
+        tooltipTitleFontFamily: "'FreightSans Pro', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Geneva, Verdana, sans-serif",
+        tooltipTitleFontColor: "#2e2e32",
+        tooltipYPadding: 10,
+        tooltipXPadding: 10,
+        tooltipCaretSize: 6,
+        tooltipCornerRadius: 3,
+        tooltipXOffset: 2
+    });
+
 })();
