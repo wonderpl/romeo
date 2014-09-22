c5f3c7ac71e31bb955d8a211fa0b0282bf081970
Johan R-G <johan@wonderpl.com>
2014-09-22 15:08:34 +0100

    Make sure we test for presence of all current services

--- test-front/unit/servicesSpec.js
+++ test-front/unit/servicesSpec.js
@@ -24,6 +24,16 @@ describe('services', function(){
   	expect(AccountService.getAccount).toBeDefined();
   }));
 
+  it('should have search service', inject(function(SearchService) {
+    expect(SearchService).toBeDefined();
+    expect(SearchService.search).toBeDefined();
+  }));
+
+  it('should have user service', inject(function(UserService) {
+    expect(UserService).toBeDefined();
+    expect(UserService.getUser).toBeDefined();
+  }));
+
   it('should have collaborators service', inject(function(CollaboratorsService) {
   	expect(CollaboratorsService).toBeDefined();
   	expect(CollaboratorsService.addCollaborators).toBeDefined();
@@ -34,42 +44,70 @@ describe('services', function(){
   	expect(CommentsService.resolveComment).toBeDefined();
   }));
 
+  it('should have data service', inject(function(DataService) {
+    expect(DataService).toBeDefined();
+    expect(DataService.request).toBeDefined();
+  }));
+
+  it('should have error service', inject(function(ErrorService) {
+    expect(ErrorService).toBeDefined();
+  }));
+
+  it('should have location service', inject(function(LocationService) {
+    expect(LocationService).toBeDefined();
+    expect(LocationService.getAll).toBeDefined();
+  }));
+
   it('should have modal service', inject(function(modal) {
   	expect(modal).toBeDefined();
   	expect(modal.show).toBeDefined();
   }));
 
-  it('should have search service', inject(function(SearchService) {
-  	expect(SearchService).toBeDefined();
-  	expect(SearchService.search).toBeDefined();
+  describe('analytics services', function () {
+
+    it('should have stats service', inject(function(StatsService) {
+      expect(StatsService).toBeDefined();
+      expect(StatsService.getOne).toBeDefined();
+    }));
+
+    it('should have analytics fields service', inject(function(AnalyticsFields) {
+      expect(AnalyticsFields).toBeDefined();
+    }));
+
+    it('should have overview service', inject(function(OverviewService) {
+      expect(OverviewService).toBeDefined();
+      expect(OverviewService.get).toBeDefined();
+    }));
+
+    it('should have performance service', inject(function(PerformanceService) {
+      expect(PerformanceService).toBeDefined();
+      expect(PerformanceService.get).toBeDefined();
+    }));
+
+    it('should have geographic service', inject(function(GeographicService) {
+      expect(GeographicService).toBeDefined();
+      expect(GeographicService.get).toBeDefined();
+    }));
+
+    it('should have engagement service', inject(function(EngagementService) {
+      expect(EngagementService).toBeDefined();
+      expect(EngagementService.get).toBeDefined();
+    }));
+
+  });
+
+  it('should have tags service', inject(function(TagService) {
+    expect(TagService).toBeDefined();
+    expect(TagService.getTags).toBeDefined();
   }));
 
-  it('should have stats service', inject(function(StatsService) {
-  	expect(StatsService).toBeDefined();
-  	expect(StatsService.getOne).toBeDefined();
+  it('should have upload service', inject(function(UploadService) {
+    expect(UploadService).toBeDefined();
+    expect(UploadService.uploadVideo).toBeDefined();
   }));
 
-  it('should have analytics fields service', inject(function(AnalyticsFields) {
-  	expect(AnalyticsFields).toBeDefined();
-  }));
-
-  it('should have overview service', inject(function(OverviewService) {
-  	expect(OverviewService).toBeDefined();
-  	expect(OverviewService.get).toBeDefined();
-  }));
-
-  it('should have performance service', inject(function(PerformanceService) {
-  	expect(PerformanceService).toBeDefined();
-  	expect(PerformanceService.get).toBeDefined();
-  }));
-
-  it('should have geographic service', inject(function(GeographicService) {
-  	expect(GeographicService).toBeDefined();
-  	expect(GeographicService.get).toBeDefined();
-  }));
-
-  it('should have engagement service', inject(function(EngagementService) {
-  	expect(EngagementService).toBeDefined();
-  	expect(EngagementService.get).toBeDefined();
+  it('should have videos service', inject(function(VideoService) {
+    expect(VideoService).toBeDefined();
+    expect(VideoService.get).toBeDefined();
   }));
 });
