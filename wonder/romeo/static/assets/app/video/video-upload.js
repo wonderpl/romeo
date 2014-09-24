5b3b4be785d8a45aa1878582b7ba951a860eb4c1
Johan R-G <johan@wonderpl.com>
2014-09-24 14:00:13 +0100

    Move onFileSelect into upload directive

--- wonder/romeo/static/assets/app/video/video-upload.js
+++ wonder/romeo/static/assets/app/video/video-upload.js
@@ -6,6 +6,33 @@ angular.module('RomeoApp.directives')
   return {
     restrict : 'E',
     replace : true,
-    template : $templateCache.get('video/video-upload.dir.html')
+    template : $templateCache.get('video/video-upload.dir.html'),
+    controller: function ($scope) {
+      $scope.onFileSelect = function(files) {
+        $scope.video.title = $scope.video.title || stripExtension(files[0].name);
+        var data = { title : $scope.video.title };
+
+        $scope.$emit('notify', {
+          status : 'info',
+          title : 'Uploading Video',
+          message : 'Upload started for video ' + $scope.video.title + '.'}
+        );
+
+        if ($scope.video.id) {
+          persistVideoData(data);
+          UploadService.uploadVideo(files[0], $scope.video.id);
+        } else {
+          VideoService.create(data).then(function (data) {
+            persistVideoData(data);
+            $scope.flags.isOwner = true;
+            UploadService.uploadVideo(files[0], data.id);
+            $rootScope.uploadingVideoId = data.id;
+          });
+        }
+        $rootScope.isUploadingOrProcessingTemp = true;
+        $scope.showPreviewSelector = true;
+        $scope.flags.showUpload = false;
+      };
+    }
   };
 }]);
