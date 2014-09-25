55d4cca68b4aced21f20d495a2854bb822ea82e0
Johan R-G <johan@wonderpl.com>
2014-09-25 17:04:13 +0100

    Fix avatar mis-alignment on public video

--- wonder/romeo/static/assets/app/video/edit/video-search-terms.js
+++ wonder/romeo/static/assets/app/video/edit/video-search-terms.js
@@ -2,50 +2,50 @@
   'use strict';
 
   function VideoSearchTerms($templateCache) {
-      return {
-        restrict : 'E',
-        replace : true,
-        template : $templateCache.get('video/edit/video-search-terms.dir.html'),
-        scope: true,
-        controller : function ($scope) {
-          $scope.select2Options = {
-            'multiple': true,
-            'simple_tags': true,
-            'width': '100%',
-            'minimumInputLength': 3,
-            'allowClear': true,
-            'tokenSeparators': [','],
-            'ajax': {
-              url: '/api/search_keywords',
-              cache: true,
-              quietMillis: 500,
-              data: function (term, page) {
-                return {prefix: term, size: 10, start: (page - 1) * 10};
-              },
-              results: function (data, page) {
-                var result = {results: [], more: false};
-                $.each(data.search_keyword.items || [], function() {
-                  result.results.push({id: this, text: this});
-                });
-                if (data.search_keyword.items.length == 10) {
-                  result.more = true;
-                }
-                return result;
-              }
+    return {
+      restrict : 'E',
+      replace : true,
+      template : $templateCache.get('video/edit/video-search-terms.dir.html'),
+      scope: true,
+      controller : function ($scope) {
+        $scope.select2Options = {
+          'multiple': true,
+          'simple_tags': true,
+          'width': '100%',
+          'minimumInputLength': 3,
+          'allowClear': true,
+          'tokenSeparators': [','],
+          'ajax': {
+            url: '/api/search_keywords',
+            cache: true,
+            quietMillis: 500,
+            data: function (term, page) {
+              return {prefix: term, size: 10, start: (page - 1) * 10};
             },
-            createSearchChoice: function (term) {
-              return {id: term, text: term};
-            },
-            'initSelection': function (element, callback) {
-              var data = [];
-              $(element.val().split(",")).each(function () {
-                data.push(this);
+            results: function (data, page) {
+              var result = {results: [], more: false};
+              $.each(data.search_keyword.items || [], function() {
+                result.results.push({id: this, text: this});
               });
-              callback(data);
+              if (data.search_keyword.items.length == 10) {
+                result.more = true;
+              }
+              return result;
             }
-          };
-        }
-      };
+          },
+          createSearchChoice: function (term) {
+            return {id: term, text: term};
+          },
+          'initSelection': function (element, callback) {
+            var data = [];
+            $(element.val().split(",")).each(function () {
+              data.push(this);
+            });
+            callback(data);
+          }
+        };
+      }
+    };
   }
   angular.module('RomeoApp.directives')
     .directive('videoSearchTerms', ['$templateCache', VideoSearchTerms]);
