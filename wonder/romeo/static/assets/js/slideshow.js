52c674ae237af4ad3e402dec4c7c744f9effe6fe
Johan R-G <johan@wonderpl.com>
2014-09-30 17:15:50 +0100

    First working version of request beta invite

--- wonder/romeo/static/assets/js/slideshow.js
+++ wonder/romeo/static/assets/js/slideshow.js
@@ -3,6 +3,7 @@ new IdealImageSlider.Slider('#slider');
 
 // smoothscroll
 window.smoothScrollTo = (function () {
+  'use strict';
   var timer, start, factor;
 
   return function (target, duration) {
@@ -38,7 +39,7 @@ window.smoothScrollTo = (function () {
   function toogleClass(elem, className) {
     var tempClassName = ' ' + elem.className + ' ';
     if (tempClassName.indexOf(' ' + className + ' ') !== -1)
-      elem.className.replace(className, ''); // Set??
+      elem.className.replace(className, '');
     else
       elem.className += ' ' + className;
   }
@@ -52,4 +53,87 @@ window.smoothScrollTo = (function () {
   document.getElementById('js-app-tray-trigger').addEventListener('click', function () {
     toggleClass(document.getElementById('page-body'), 'tray-open');
   });
+  document.getElementById('invite-submit').addEventListener('click', function (event) {
+    registerInterest();
+    return false;
+  });
+
+  function registerInterest() {
+    // var name = document.getElementById('invite-name').value;
+    var email = document.getElementById('invite-email').value;
+    // var msg = document.getElementById('invite-message').value;
+    ajaxPost('/api/invite_request', {
+      'email': email,
+      // 'name': name,
+      // 'message': msg
+    }, function () {
+      notify('Success', 'Invitation sent');
+      clearForm();
+    }, function (res) {
+      console.error('Failed ajax post: ', res);
+      notify('Failed', 'Invitation failed');
+      clearForm();
+    });
+  }
+
+  function clearForm() {
+    // document.getElementById('invite-name').value = '';
+    document.getElementById('invite-email').value = '';
+    // document.getElementById('invite-message').value = '';
+  }
+
+  function notify(state, msg) {
+    var elem = document.getElementById('invite-form').insertAdjacentHTML('afterend', '<div class="notification ' + state + '"><h2>' + state + '</h2> ' + msg + '</div>');
+    setTimeout(function () {
+      elem.parentElement.removeChild(elem);
+    }, 3000);
+  }
+
+  function ajaxPost(url, data, success, failure) {
+    var xhr;
+
+    success = success || function () {};
+    failure = failure || function () {};
+
+    if (typeof XMLHttpRequest !== 'undefined')
+      xhr = new XMLHttpRequest();
+    else {
+      var versions = ["MSXML2.XmlHttp.5.0",
+                      "MSXML2.XmlHttp.4.0",
+                      "MSXML2.XmlHttp.3.0",
+                      "MSXML2.XmlHttp.2.0",
+                      "Microsoft.XmlHttp"];
+
+      for(var i = 0, len = versions.length; i < len; i++) {
+        try {
+          xhr = new ActiveXObject(versions[i]);
+          break;
+        }
+        catch(e){}
+      } // end for
+    }
+
+    xhr.onreadystatechange = ensureReadiness;
+
+    function ensureReadiness() {
+      if(xhr.readyState < 4) {
+        return;
+      }
+
+
+      // all is well
+      if (xhr.readyState === 4) {
+        if (xhr.status !== 200 && xhr.status !== 204)
+          failure(xhr);
+        else
+          success(xhr);
+      }
+    }
+
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(data ? JSON.stringify(data) : '');
+  }
 })();
+
+
